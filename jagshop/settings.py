e5eb59e55ac6a7a85a3dd84bf32b26a1612dcf8a
marianochavez <amarianochavez@gmail.com>
2021-07-04 15:56:47 -0300

    debug

--- jagshop/settings.py
+++ jagshop/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DJANGO_DEBUG','True')
 
 ALLOWED_HOSTS = ['jaguaretekaaecommerce.herokuapp.com','localhost']
 
